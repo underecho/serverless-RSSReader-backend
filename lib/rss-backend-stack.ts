8ed8426e124e34bdf16872ec5844936ae6a250f1
under <poke2000@live.jp>
2022-06-29 15:55:06 +0900

    prepare develop lambda func

--- lib/rss-backend-stack.ts
+++ lib/rss-backend-stack.ts
@@ -1,16 +1,38 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
 
 export class RssBackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define RSS Article table
+    const rssArticleTable = new dynamodb.Table(this, 'Channel', {
+      partitionKey: { name: 'ArticleUrl', type: dynamodb.AttributeType.STRING },
+      sortKey:{ name: 'SourceUrl', type: dynamodb.AttributeType.STRING}
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RssBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    rssArticleTable.addLocalSecondaryIndex({
+      indexName: "ByPubDate",
+      sortKey: {
+        name: "pubDate",
+        type: dynamodb.AttributeType.NUMBER
+      }
+    });
+
+    const tableName = rssArticleTable.tableName;
+
+    const slashLambdaFunc = new PythonFunction(this, 'slashFunc', {
+      entry: './src/getFeed/',
+      timeout: cdk.Duration.seconds(180),
+      runtime: Runtime.PYTHON_3_8,
+      index: 'lambda_getFeed.py',
+      environment: {'TABLE_NAME':tableName}
+
+    });
   }
 }
